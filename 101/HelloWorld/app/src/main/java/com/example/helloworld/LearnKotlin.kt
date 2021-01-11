af9b911a288f90553ed4a61337d4e38658b094f4
loongse <18707950895@163.com>
2021-01-11 10:59:59 +0800

    if语句对比

--- 101/HelloWorld/app/src/main/java/com/example/helloworld/LearnKotlin.kt
+++ 101/HelloWorld/app/src/main/java/com/example/helloworld/LearnKotlin.kt
@@ -17,3 +17,17 @@ fun main() {
 //fun largeNumber(a: Int, b: Int): Int = max(a, b)
 //自动类型推导简写
 fun largeNumber(a: Int, b: Int) = max(a, b)
+
+//使用if语句
+//fun largeNumIf(a: Int, b: Int): Int {
+//    if (a > b) return a
+//    else return b
+//}
+//return if语句的结果
+//fun largeNumIf(a: Int, b: Int): Int {
+//    return if (a > b) a
+//    else b
+//}
+
+//只有一句直接省略
+fun largeNumIf(a: Int, b: Int) = if (a > b) a else b
