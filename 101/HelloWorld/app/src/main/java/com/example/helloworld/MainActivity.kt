2d83c86de5b928e15cb86c210dde7046c5ac51a3
loongse <18707950895@163.com>
2021-01-25 20:10:13 +0800

    kotlin变量延迟初始化，解决某些场景下可确定不为空变量的延迟赋值

--- 101/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ 101/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    //对变量延迟初始化,使用lateinit关键字，告诉kotlin编译器在未来对变量进行初始化，可以不必赋值null，从而减少后续的判空操作，如下
+    private lateinit var str: String
 
+    //不适用延迟初始化，则需要先赋值null，随后还需要判空等操作
+    private var strNor: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("data", "onCreate execute")
+        if (!::str.isInitialized) {
+            //判断是否初始化
+//            实现相关操作
+            str = "延迟初始化"
+        }
+        strNor = "未延迟初始化"
+    }
+
+    override fun onClick(v: View?) {
+        //一个使用了延迟初始化，一个未使用
+        str[0]//不需要判空操作
+        strNor?.get(0)//需要判空操作
     }
 
 }
